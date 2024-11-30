0b828594c889e136a0626ea9062493b8a0938cd7
Thuy Pham <thuypham170109@gmail.com>
2024-11-30 21:46:52 +0700

    fix set content type

--- middleware.go
+++ middleware.go
@@ -1,9 +1,13 @@
 package helpers
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func SetContentTypeMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-	})
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
 }
