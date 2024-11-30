e52cc7f84219d0c67fda5f22b1033d4801b24484
Thuy Pham <thuypham170109@gmail.com>
2024-11-30 21:26:00 +0700

    add json middleware

---
+++ middleware.go
@@ -0,0 +1,9 @@
+package helpers
+
+import "net/http"
+
+func SetContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+	})
+}
